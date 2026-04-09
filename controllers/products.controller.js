d51397bf1da109e57dfcff519610c16117b82602
Rafael Grisante Dallaqua <rafaeldallaquadev@gmail.com>
2026-04-08 22:42:25 -0300

    organized controller and services files

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,7 +1,7 @@
 //controllers/products.controller.js
 
 import {pool} from '../db.js'
-
+import * as services from '../services/products.services.js'
 
 export async function newProduct(req, res) {
     try {
@@ -9,75 +9,35 @@ export async function newProduct(req, res) {
         const userId = req.user.id
         const description = req.body.description?.trim() || ""
 
-        const [result] = await pool.execute(
-            'INSERT into products (name, description, price, stock, user_id) values (?, ?, ?, ?, ?)',
-            [name, description.trim(), price, stock, userId]
-        )
+        const product = await services.createProduct(name, price, stock, userId, description)
 
-        return res.status(201).json({message: "Produto cadastrado com sucesso",
-            id: result.insertId,
-            name,
-            description, 
-            price, 
-            stock,
-            userId
+        return res.status(201).json({
+            message: "Produto cadastrado com sucesso",
+            data: product
         });
     } catch (err) {
         console.log(err);
         return res.status(500).json({error: "Não foi possível cadastrar produto"})
     }
 }
+
+
 export async function updateProduct (req, res) {
     try {
 
         const id = req.params.id;
         const userId = req.user.id;
-        let query = 'UPDATE products SET '
-        const queryArray = []
-        const queryParams = []
-
         const {name, description, price, stock} = req.body
+        const product = await services.productPatch(id, userId, name, description, price, stock)
         
-        if (name !== undefined) {
-            queryArray.push('name = ?')
-            queryParams.push(name.trim());
-        }
-
-        if (description !== undefined) {
-            queryArray.push('description = ?')
-            queryParams.push(description.trim())
-        }
-
-        if (price !== undefined) {
-            queryArray.push('price = ?')
-            queryParams.push(price)
-        }
-
-        if (stock !== undefined) {
-            queryArray.push('stock = ?')
-            queryParams.push(stock);
-        }
-
-        const queryPart = queryArray.join(', ')
-        query = query + queryPart + ' WHERE id = ? AND user_id = ?'
-
-        const [result] = await pool.execute(query, [...queryParams, id, userId]);
-
-        if (result.affectedRows === 0) {
-            return res.status(404).json({error: "Produto não encontrado"})
-        }
-
-        const [rows] = await pool.execute('select id, name, description, price, stock from products where id = ? and user_id = ?',
-            [id, userId])
-
         return res.status(200).json({
             message: "Produto atualizado com sucesso",
-            data: rows[0]
+            data: product
         });
 
     } catch (err) {
-        console.log(err);
-        return res.status(500).json({ error: "Não foi possível atualizar produto" });
+        return res.status(err.status || 500).json({
+            error: err.message || "Erro no servidor"})
     }
 }
 
@@ -85,32 +45,18 @@ export async function deleteProduct(req, res) {
     try {
         const id = req.params.id
         const userId = req.user.id
-        const [rows] = await pool.execute(
-            'SELECT id, name from products where id = ? AND user_id = ?',
-            [id, userId]
-        )
-
-        const product = rows[0];
-
-        if (rows.length === 0) {
-            return res.status(404).json({error: "Não foi possível encontrar o produto"})
-        }
-
-        const [result] = await pool.execute(
-            'delete from products where id = ? AND user_id = ?',
-            [id, userId]
-        )
+        const product = await services.productRemove(id, userId);
 
         return res.status(200).json({
             message: "Produto removido com sucesso",
-            product
+            data: product
         })
         
     } catch (err) {
-        console.log(err);
-        return res.status(500).json({error: "Não foi possível atualizar produto"})
+        return res.status(err.status || 500).json({
+            error: err.message || "Erro no servidor"
+        })
     }
-
 }
 
 export async function listProducts(req, res) {
@@ -126,52 +72,24 @@ export async function listProducts(req, res) {
             limit = 10
         } = req.query;
         
-        const pageNumber = parseInt(page) || 1;
-        const limitNumber = parseInt(limit) || 10;
-        const offset = (pageNumber - 1) * limitNumber;
+        const products = await services.productsFilter(name, minPrice, maxPrice,
+            inStock, sortBy, order, page, limit);
 
-       
-        let query = 'SELECT id, name, description, price, stock FROM products WHERE name LIKE ? AND price BETWEEN ? AND ?';
-        const params = [`%${name}%`, parseFloat(minPrice), parseFloat(maxPrice)];
-       
-        if (inStock === 'true') {
-          query += ' AND stock > 0';
-
-        }
-        
-        const allowedSortBy = ['id', 'name', 'price'];
-        const allowedOrder = ['ASC', 'DESC'];
-        
-        const sortColumn = allowedSortBy.includes(sortBy) ? sortBy : 'id';
-        const sortOrder = allowedOrder.includes(order.toUpperCase()) ? order.toUpperCase() : 'ASC';
-       
-        query += ` ORDER BY ${sortColumn} ${sortOrder} LIMIT ${limitNumber} OFFSET ${offset}`;
-       
-        const [rows] = await pool.execute(query, params);
-       
-        return res.status(200).json({ page: parseInt(page), limit: parseInt(limit), data: rows });
+        return res.status(200).json(products);
     } catch (err) {
-        console.error(err);
-        return res.status(500).json({error: "Não foi possível listar produtos"})
+        return res.status(err.status || 500).json({error: err.message || "Erro no servidor"})
     }
 }
 
 export async function getProduct(req, res) {
     try {
         const id = req.params.id
-        const [rows] = await pool.execute(
-            'select id, name, price, stock, description from products where id = ?',
-            [id]
-        )
+        const product = await services.productByID(id);
 
-        if (rows.length === 0 ){
-            return res.status(404).json({error: "Não foi possível encontrar o produto"})
-        }
-
-        return res.status(200).json(rows);
-
+        return res.status(200).json(product);
     } catch (err) {
-        console.error(err);
-        return res.status(500).json({error: "Erro ao buscar produto, tente novamente mais tarde"})
+        return res.status(err.status || 500).json({
+            error: err.message || "Erro no servidor"
+        })
     }
 }
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,20 +1,5 @@
 ////controllers/products.controller.js
-
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { pool } from '../db.js'
-import dotenv from 'dotenv'
-dotenv.config()
-
-
-const JWT_SECRET = process.env.JWT_SECRET
-
-if (!JWT_SECRET) {
-    throw new Error("JWT_SECRET não definido");
-}
-
-
-const saltOrRounds = 10
+import * as services from '../services/user.services.js'
 
 export async function register(req, res){
     try {
@@ -24,33 +9,16 @@ export async function register(req, res){
             return res.status(400).json({error: "Campos obrigatórios"})
         }
 
-        const emailNormalized = email.trim().toLowerCase()
+        const user = await services.registerUser(name, email, password);
 
-        const hashedPassword = await bcrypt.hash(password, saltOrRounds)
-
-        const [result] = await pool.execute(
-            'insert into users (name, email, password) values (?, ?, ?)', 
-            [name, emailNormalized, hashedPassword]
-        )
-
-        return res.status(201).json({
-            message: "Usuário criado com sucesso",
-            data: {
-                id: result.insertId,
-                name, 
-                email: emailNormalized
-            }
-        })
-
-
-
+       return res.status(201).json({
+        message: "Usuário cadastrado com sucesso",
+        data: user
+       })
     }catch (err) {
-        if (err.code === 'ER_DUP_ENTRY') {
-            return res.status(400).json({ error: "Email já cadastrado" })
-        }
-        
-        console.error(err);
-        res.status(500).json({error: "Erro no servidor"})
+        res.status(err.status || 500).json({
+            error: err.message || "Erro no servidor"
+        });
     }
 }
 
@@ -58,31 +26,15 @@ export async function register(req, res){
 export async function login(req, res) {
     try {
         const {email, password} = req.body
-        const [rows] = await pool.execute(
-            'SELECT * from users where email = ?',
-            [email]
-        )
 
-        if (rows.length === 0){
-            return res.status(404).json({error:"Email ou senha inválidos"})
-        }
+        const userToken = await services.userLogin(email, password)
 
-        const user = rows[0];
-
-        const match = await bcrypt.compare(password, user.password);
-
-        if (!match){
-            return res.status(404).json({error:"Email ou senha inválidos"})
-        }
-
-        const token = jwt.sign({id: user.id, name: user.name, email: user.email, role: user.role}, JWT_SECRET , {expiresIn: '7d'})
-
-        return res.status(200).json({accessToken: token})
+        return res.status(200).json(userToken)
         
     }catch (err) {
-        console.error(err);
-        return res.status(500).json({error: "Não foi possível fazer o login"})
+        return res.status(err.status || 500).json({
+            error: err.message || "Erro no servidor"
+        })
     }
-
-
 }
+
