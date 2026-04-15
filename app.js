ff995764ccfca26b8cac0d544da2ce05d593d030
Rafael Grisante Dallaqua <rafaeldallaquadev@gmail.com>
2026-04-15 07:44:34 -0300

    ready for deploy

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ import wishlistRouter from './routes/wishlist.routes.js'
 import cartRouter from './routes/cart.routes.js'
 import { errorMiddleware } from './middlewares/error.middlewares.js'
 import authRouter from './routes/auth.routes.js'
-
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 
 app.use('/user', userRouter);
@@ -18,8 +19,8 @@ app.use('/cart', cartRouter);
 app.use('/auth', authRouter)
 
 app.use(errorMiddleware);
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000")
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
 })
